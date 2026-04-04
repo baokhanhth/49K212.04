5a4a65d433b6de2dd070ee47381bf78e6d295728
tanphuc57 <tanphucnguyencong@gmail.com>
2026-04-04 13:15:00 +0700

    fix: resolve conflict auth.module

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,18 +1,21 @@
 import { Module } from '@nestjs/common';
-<<<<<<< HEAD
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './jwt.strategy';
-import { NguoiDung } from '../nguoi-dung/entities/nguoi-dung.entity';
 import { TokenBlacklistService } from './token-blacklist.service';
+
+import { NguoiDung } from '../nguoi-dung/entities/nguoi-dung.entity';
+import { OtpKhoiPhucMatKhau } from './entities/otp-khoi-phuc.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([NguoiDung]),
+    ConfigModule,
+    TypeOrmModule.forFeature([NguoiDung, OtpKhoiPhucMatKhau]),
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.registerAsync({
       imports: [ConfigModule],
@@ -27,25 +30,6 @@ import { TokenBlacklistService } from './token-blacklist.service';
   ],
   controllers: [AuthController],
   providers: [AuthService, JwtStrategy, TokenBlacklistService],
-  exports: [AuthService, JwtStrategy, PassportModule, PassportModule, TokenBlacklistService], 
-})
-export class AuthModule {}
-=======
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
-
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
-import { NguoiDung } from '../nguoi-dung/entities/nguoi-dung.entity';
-import { OtpKhoiPhucMatKhau } from './entities/otp-khoi-phuc.entity';
-
-@Module({
-  imports: [
-    ConfigModule,
-    TypeOrmModule.forFeature([NguoiDung, OtpKhoiPhucMatKhau]),
-  ],
-  providers: [AuthService],
-  controllers: [AuthController],
+  exports: [AuthService, JwtStrategy, PassportModule, TokenBlacklistService],
 })
 export class AuthModule { }
->>>>>>> feat/US-04-BE
